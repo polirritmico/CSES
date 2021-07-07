6f740f196b7c6f65a992e12088ba757642e307e8
Eduardo Bray <ebray187@gmail.com>
2021-07-06 22:58:06 -0400

    Update 11.py

--- 11.py
+++ 11.py
@@ -2,16 +2,16 @@ def solve(input_str):
 	input_str = input_str.split()
 	pile_a = int(input_str[0])
 	pile_b = int(input_str[1])
- 
+
 	if pile_b > pile_a:
 		pile_a, pile_b = pile_b, pile_a
- 
+
 	if pile_a <= 2 * pile_b:
 		a = pile_a % 3
 		b = pile_b % 3
 		if b > a:
 			a, b = b, a
- 
+
 		if (a == b and a == 0) or (a == 2 * b and a != 1):
 			return "YES"
 	return "NO"
